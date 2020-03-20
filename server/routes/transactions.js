92b6cecdc019eebee380503d5000f205cb48b517
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-20 16:58:53 +0100

    routes & controllers set up

---
+++ server/routes/transactions.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getTransactions,
+  addTransaction,
+  deleteTransaction
+} = require("../controllers/transactions");
+
+router.route("/").get(getTransactions);
+
+module.exports = router;
