7bcc6821737fb14a710c3bb3d740f66cff156f3b
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-20 17:28:55 +0100

    add transaction controller added

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -12,7 +12,7 @@ exports.getTransactions = async (req, res, next) => {
       data: transactions
     });
   } catch (error) {
-    return res.send(500).json({
+    return res.status(500).json({
       success: false,
       error: "Server Error"
     });
@@ -23,7 +23,27 @@ exports.getTransactions = async (req, res, next) => {
 // @route   POST   /api/v1/transactions
 // @access  Public
 exports.addTransaction = async (req, res, next) => {
-  res.send("POST transactions");
+  try {
+    const { text, amount } = req.body;
+    const transaction = await Transaction.create(req.body);
+    return res.status(201).json({
+      success: true,
+      data: transaction
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const messages = Object.values(error.errors).map(val => val.message);
+      return res.status(400).json({
+        success: false,
+        error: messages
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: "Server Error"
+      });
+    }
+  }
 };
 
 // @desc    Delete transaction
