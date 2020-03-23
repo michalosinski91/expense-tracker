c3cd7fe6c999b6bc9e6826c725535435b7d76b4e
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-23 15:07:01 +0100

    spinner added/implemented on transaction list

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Spinner() {
+  return (
+    <div className="spinner">
+      <div className="spinner__circle"></div>
+      <div className="spinner__circle"></div>
+      <div className="spinner__circle"></div>
+    </div>
+  );
+}
--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect } from "react";
 import TransactionItem from "./TransactionItem";
+import Spinner from "./Spinner";
 import { GlobalContext } from "../context/GlobalState";
 
 export default function TransactionList() {
-  const { transactions, getTransactions } = useContext(GlobalContext);
+  const { transactions, getTransactions, loading } = useContext(GlobalContext);
 
   useEffect(() => {
     getTransactions();
@@ -12,16 +13,20 @@ export default function TransactionList() {
   return (
     <div className="history">
       <h3 className="heading--secondary">History</h3>
-      <ul className="history__list">
-        {transactions.map(item => (
-          <TransactionItem
-            key={item._id}
-            id={item._id}
-            text={item.text}
-            amount={item.amount}
-          />
-        ))}
-      </ul>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <ul className="history__list">
+          {transactions.map(item => (
+            <TransactionItem
+              key={item._id}
+              id={item._id}
+              text={item.text}
+              amount={item.amount}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
