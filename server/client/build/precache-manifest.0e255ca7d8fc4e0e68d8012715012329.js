27e0da82598ec2ee26097cf95b5343e9c8423a41
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-23 14:35:54 +0100

    production build process added

---
+++ server/client/build/precache-manifest.0e255ca7d8fc4e0e68d8012715012329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8edc692a9d4580461433eec7672b2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c95a446c07b75184c53",
+    "url": "/static/css/main.86dfd061.chunk.css"
+  },
+  {
+    "revision": "ac19102d64653faf418d",
+    "url": "/static/js/2.fd401636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd401636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c95a446c07b75184c53",
+    "url": "/static/js/main.9f74d493.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
